55a0b63d0a7a09d5ac5797ea6bf7683648c5d0da
Sky Luo <skylaw0424@icloud.com>
2022-09-18 20:10:04 -0400

    added codes to  log the number of elements in portrait

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,26 @@
 
 /* javascript */
+let matches = document.querySelectorAll("div");
+let length = matches.length;
+console.log("The total number of HTML elements used in the portraint is: " + length);
 
-var bauhaus = `
 
-Staaatliches Bauhaus (1919-1933)
 
-      ..-""T-..
-    ,'__   |   '.
-   /   N|  |     !
-  :     '  |      :
-  [      __|      ]
-  :      _I       :
-   !   ___I      /
-    '.  N]     ,'
-      "-"!..--"
 
-`;
-
-console.log(bauhaus);
+// var bauhaus = `
+//
+// Staaatliches Bauhaus (1919-1933)
+//
+//       ..-""T-..
+//     ,'__   |   '.
+//    /   N|  |     !
+//   :     '  |      :
+//   [      __|      ]
+//   :      _I       :
+//    !   ___I      /
+//     '.  N]     ,'
+//       "-"!..--"
+//
+// `;
+//
+// console.log(bauhaus);
