2894d7e21a2a7d39dc89345a694f96a424d8bf5c
Sky Luo <skylaw0424@icloud.com>
2022-09-18 20:54:46 -0400

    added cursor coordinates display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,16 @@
 
 /* javascript */
+
+//the following return the number of portrait elements (div)
 let matches = document.querySelectorAll("div");
 let length = matches.length;
 console.log("The total number of HTML elements used in the portraint is: " + length);
+
+//the following track the mouse and return coordinates (x and y) of the cursor
+window.addEventListener('mousemove', function(e){
+  document.getElementById('x-value').textContent = e.x;
+  document.getElementById('y-value').textContent = e.y;
+});
 
 
 
